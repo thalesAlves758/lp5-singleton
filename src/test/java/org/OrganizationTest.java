78b4ce38ceeed02f6b25770614e814cfcabbb9e4
thalesAlves758 <thalesalves758@gmail.com>
2023-03-16 23:29:46 -0300

    test: add should return organization name test

--- src/test/java/org/OrganizationTest.java
+++ src/test/java/org/OrganizationTest.java
@@ -1,4 +1,23 @@
 package org;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class OrganizationTest {
+    Organization organization;
+
+    @BeforeAll
+    void setUp() {
+        organization = Organization.getInstance();
+    }
+
+    @Test
+    public void shouldReturnOrganizationName() {
+        organization.setName("Alguma Organização");
+        assertEquals(organization.getName(), "Alguma Organização");
+    }
 }
